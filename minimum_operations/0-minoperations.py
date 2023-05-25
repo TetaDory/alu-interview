c00e3b1c5dbe0c0e62947133a7fbd517cee6778a
TetaDory <d.rutayisir1@alustudent.com>
2023-05-25 11:00:20 -0700

    f

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,19 +4,14 @@ def minOperations(n):
     if n <= 1:
         return 0
     
-    operations = 0
+    operation = 0
     divisor = 2
 
     while n > 1:
         if n % divisor == 0:
             n //= divisor
-            operations += divisor
+            operation += divisor
         else:
             divisor += 1
     
-    return operations
-
-# Example usage
-n = 9
-result = minOperations(n)
-print("Number of operations:", result)
+    return operation
