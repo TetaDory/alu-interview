841892348fd1321807461ce1f1632c096f7a961a
TetaDory <d.rutayisir1@alustudent.com>
2023-06-16 05:05:32 -0700

    0-rain.py

---
+++ rain/0-rain.py
@@ -0,0 +1,25 @@
+def rain(walls):
+    if len(walls) == 0:
+        return 0
+
+    total_water = 0
+    left_max = 0
+    right_max = 0
+    left_pointer = 0
+    right_pointer = len(walls) - 1
+
+    while left_pointer < right_pointer:
+        if walls[left_pointer] < walls[right_pointer]:
+            if walls[left_pointer] > left_max:
+                left_max = walls[left_pointer]
+            else:
+                total_water += left_max - walls[left_pointer]
+            left_pointer += 1
+        else:
+            if walls[right_pointer] > right_max:
+                right_max = walls[right_pointer]
+            else:
+                total_water += right_max - walls[right_pointer]
+            right_pointer -= 1
+
+    return total_water
